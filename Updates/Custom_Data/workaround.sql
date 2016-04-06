2ddc783ba4a3312bb6db426f990b85f9cf104e9f
Lankytim <tim@happyplace.net.au>
2016-04-06 18:55:46 +1000

    Added workaround for stuck creature

--- Updates/Custom_Data/workaround.sql
+++ Updates/Custom_Data/workaround.sql
@@ -1,1 +1,4 @@
 UPDATE creature_template set DamageMultiplier=1 where DamageMultiplier=0;
+
+#Stuck Withered Bog Lord in Zangarmarsh
+UPDATE `mangos`.`creature` SET `position_x` = '420.596', `position_y` = '5488.030273', `position_z` = '24.060814' WHERE `creature`.`guid` = 69278;
